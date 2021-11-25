a98bcb3d5196ab8f326006a3e8a904f0009a02d9
Stanislav Tataren <79329397+tatarenstas@users.noreply.github.com>
2021-11-25 20:44:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,15 +4,15 @@ lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
 from keras.models import load_model
-model = load_model('/content/Simple-Python-Chatbot/chatbot_model.h5')
+model = load_model('chatbot_model.h5')
 import json
 import random
 
 nltk.download('punkt')
 nltk.download('wordnet')
-intents = json.loads(open('/content/Simple-Python-Chatbot/intents.json').read())
-words = pickle.load(open('/content/Simple-Python-Chatbot/words.pkl','rb'))
-classes = pickle.load(open('/content/Simple-Python-Chatbot/classes.pkl','rb'))
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
 
 
 def clean_up_sentence(sentence):
